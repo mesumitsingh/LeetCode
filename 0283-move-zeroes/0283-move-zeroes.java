aae1904a2a2afcbd26bdfc7a5c034678408b9be6
Sumit Kumar <me.ss.sumit.singh@gmail.com>
2024-07-11 22:55:53 +0530

    Time: 1 ms (100.00%), Space: 45.8 MB (65.77%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int temp=0; 
+        
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != 0){
+                nums[temp++] = nums[i];
+            }
+        }
+
+        for(int i=temp; i<nums.length; i++){
+            nums[i] = 0; 
+        }
+    }
+}
