7828f9472eaa29cdd25a5cd6b835d583a6109ff7
Sumit Singh <144142094+mesumitsingh@users.noreply.github.com>
2025-01-04 21:41:21 +0530

    commit changes

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
@@ -1,16 +1,19 @@
-class Solution {
-  public int waysToSplitArray(int[] nums) {
-    int ans = 0;
-    long prefix = 0;
-    long suffix = Arrays.stream(nums).asLongStream().sum();
-
-    for (int i = 0; i < nums.length - 1; ++i) {
-      prefix += nums[i];
-      suffix -= nums[i];
-      if (prefix >= suffix)
-        ++ans;
+public class Solution {
+    public int waysToSplitArray(int[] nums) {
+        int suffix = 0;
+        for(int i=0; i<nums.length; i++){ 
+            suffix += nums[i];
+        }
+        
+        int prefix = 0, count = 0;
+        for(int i=0; i<nums.length-1; i++){ 
+            prefix += nums[i]; 
+            suffix -= nums[i]; 
+            if(prefix >= suffix){ 
+                count++;
+            }
+        }
+        
+        return count;
     }
-
-    return ans;
-  }
 }
