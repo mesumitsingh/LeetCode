7f5326ba08617961ed7d54399c99aef0a602030d
Sumit Singh <144142094+mesumitsingh@users.noreply.github.com>
2026-01-05 20:11:03 +0530

    Time: 2 ms (98.85%), Space: 44.3 MB (68.39%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int st=0, end=s.length()-1;
+
+        while(st<end) {
+            if(!Character.isLetterOrDigit(s.charAt(st))) { 
+                st++;
+                continue; 
+            }
+            if(!Character.isLetterOrDigit(s.charAt(end))) { 
+                end--;
+                continue; 
+            }
+
+            if(Character.toLowerCase(s.charAt(st)) != Character.toLowerCase(s.charAt(end))) return false; 
+
+            st++; 
+            end--; 
+
+        }
+
+        return true; 
+    }
+}
