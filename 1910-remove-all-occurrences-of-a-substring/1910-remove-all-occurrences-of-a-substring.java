3eaad2e79a46dad03b9c19a081b41c41177a2aa7
Sumit Singh <144142094+mesumitsingh@users.noreply.github.com>
2026-01-13 23:41:37 +0530

    Time: 6 ms (44.6%), Space: 43.6 MB (76.47%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        StringBuilder sb = new StringBuilder();
+        int m = part.length();
+
+        for (char c : s.toCharArray()) {
+            sb.append(c);
+
+            if (sb.length() >= m) {
+                boolean match = true;
+                for (int i = 0; i < m; i++) {
+                    if (sb.charAt(sb.length() - m + i) != part.charAt(i)) {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match) {
+                    sb.delete(sb.length() - m, sb.length());
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
