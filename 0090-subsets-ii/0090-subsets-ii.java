b3f3a6957b8468269089466af4f0dd4ec5c21332
Sumit Singh <144142094+mesumitsingh@users.noreply.github.com>
2026-02-21 17:40:32 +0530

    Time: 3 ms (55.17%), Space: 45.2 MB (61.06%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public void getUniqueSubsets(int[] nums, List<List<Integer>> allSubsets, List<Integer> ans, int i) {
+        Arrays.sort(nums); 
+        if (i == nums.length) {
+            allSubsets.add(new ArrayList<>(ans));
+            return;
+        }
+
+        // include 
+        ans.add(nums[i]);
+        getUniqueSubsets(nums, allSubsets, ans, i + 1);
+
+        // backtrack
+        ans.remove(ans.size() - 1);
+
+        // exclude
+        // exclude duplicates
+        int idx = i + 1;
+        while (idx < nums.length && nums[idx] == nums[idx - 1]) {
+            idx++;
+        }
+
+        getUniqueSubsets(nums, allSubsets, ans, idx);
+
+    }
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> allSubsets = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+
+        getUniqueSubsets(nums, allSubsets, ans, 0);
+
+        return allSubsets;
+    }
+}
