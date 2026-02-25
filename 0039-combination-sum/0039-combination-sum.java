d75884889fe0d73a91ff3aff37c1b7c9d4f5f661
Sumit Singh <144142094+mesumitsingh@users.noreply.github.com>
2026-02-25 11:32:23 +0530

    Time: 10 ms (5.47%), Space: 46 MB (19.6%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,8 +1,6 @@
 class Solution {
-
-    public Set<List<Integer>> set = new HashSet<>();
     
-    public void getAllCombinations(int[] arr, int idx, int target, List<Integer> combination, List<List<Integer>> ans) { 
+    public void getAllCombinations(int[] arr, int idx, int target, List<Integer> combination, List<List<Integer>> ans, Set<List<Integer>> set) { 
 
         if(idx==arr.length || target<0) { 
             return;
@@ -17,20 +15,21 @@ class Solution {
         }
 
         combination.add(arr[idx]); 
-        getAllCombinations(arr, idx+1, target-arr[idx], combination, ans); // single choice
-        getAllCombinations(arr, idx, target-arr[idx], combination, ans);  // multiple choice
+        getAllCombinations(arr, idx+1, target-arr[idx], combination, ans, set); // single choice
+        getAllCombinations(arr, idx, target-arr[idx], combination, ans, set);  // multiple choice
 
         combination.remove(combination.size()-1); // backtracking 
 
-        getAllCombinations(arr, idx+1, target, combination, ans); // don't include the element
+        getAllCombinations(arr, idx+1, target, combination, ans, set); // don't include the element
 
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<Integer> combination = new ArrayList<>(); 
         List<List<Integer>> ans = new ArrayList<>(); 
+        Set<List<Integer>> set = new HashSet<>();
 
-        getAllCombinations(candidates, 0, target, combination, ans);
+        getAllCombinations(candidates, 0, target, combination, ans, set);
 
         return ans;
     }
