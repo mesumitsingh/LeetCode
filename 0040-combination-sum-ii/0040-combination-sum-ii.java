964460a6a0992564e924e56f90f0bf73df2d3438
Sumit Singh <144142094+mesumitsingh@users.noreply.github.com>
2026-04-11 15:37:17 +0530

    Time: 8 ms (15.21%), Space: 45.2 MB (71.97%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,30 +1,28 @@
 class Solution {
-    private List<List<Integer>> ans = new ArrayList<>();
-    private List<Integer> t = new ArrayList<>();
-    private int[] candidates;
-
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
-        this.candidates = candidates;
-        dfs(0, target);
-        return ans;
-    }
-
-    private void dfs(int i, int s) {
-        if (s == 0) {
-            ans.add(new ArrayList<>(t));
+    public void solve(int[] candidates, int target, List<Integer> curr, List<List<Integer>> result, int idx) {
+        if (target < 0) {
             return;
         }
-        if (i >= candidates.length || s < candidates[i]) {
+        if (target == 0) {
+            result.add(new ArrayList<>(curr));
             return;
         }
-        for (int j = i; j < candidates.length; ++j) {
-            if (j > i && candidates[j] == candidates[j - 1]) {
-                continue;
+        
+        for (int i = idx; i < candidates.length; i++) {
+            if (i > idx && candidates[i] == candidates[i - 1]) {
+                continue; // Ignore duplicate elements
             }
-            t.add(candidates[j]);
-            dfs(j + 1, s - candidates[j]);
-            t.remove(t.size() - 1);
+            curr.add(candidates[i]);
+            solve(candidates, target - candidates[i], curr, result, i + 1);
+            curr.remove(curr.size() - 1);
         }
+    }
+    
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        Arrays.sort(candidates); // Sort to handle duplicates
+        solve(candidates, target, curr, result, 0);
+        return result;
     }
 }
