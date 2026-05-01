ccb257f5b789f3744e3f2bab1caf6edf38c07a1b
Sumit Singh <144142094+mesumitsingh@users.noreply.github.com>
2026-05-01 23:23:49 +0530

    Time: 0 ms (100%), Space: 53 MB (20.59%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -6,22 +6,17 @@ class Solution {
             int mid = left + (right - left) / 2;
 
             if ((mid == 0 || nums[mid] != nums[mid - 1]) &&
-                (mid == nums.length - 1 || nums[mid] != nums[mid + 1])) {
+                    (mid == nums.length - 1 || nums[mid] != nums[mid + 1])) {
                 return nums[mid];
             }
 
-            if (mid % 2 == 0) {
-                if (nums[mid] == nums[mid + 1]) {
-                    left = mid + 2;
-                } else {
-                    right = mid - 1;
-                }
+            if (mid % 2 == 1)
+                mid--;
+
+            if (nums[mid] == nums[mid + 1]) {
+                left = mid + 2;
             } else {
-                if (nums[mid] == nums[mid - 1]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+                right = mid;
             }
         }
 
