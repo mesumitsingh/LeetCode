5d3cfddd9441795fdb882fdde826bcf08d50c549
Sumit Singh <144142094+mesumitsingh@users.noreply.github.com>
2026-08-03 17:24:53 +0530

    Time: 4 ms (98.5%), Space: 47.3 MB (85.31%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,55 +1,50 @@
 class Solution {
-    public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        int rows = heights.length;
-        int cols = heights[0].length;
 
-        boolean[][] pacificVisited = new boolean[rows][cols];
-        boolean[][] atlanticVisited = new boolean[rows][cols];
-
-        for (int i = 0; i < rows; i++) {
-            dfs(i, 0, heights, pacificVisited, rows, cols);
-            dfs(i, cols - 1, heights, atlanticVisited, rows, cols);
+    public void mark(int i, int j, boolean[][] arr, int oi, int oj, int[][] heights) {
+        if (i < 0 || j < 0 || i >= arr.length || j >= arr[0].length || arr[i][j]
+                || (oi >= 0 && oj >= 0 && heights[oi][oj] > heights[i][j])) {
+            return;
         }
 
-        for (int i = 0; i < cols; i++) {
-            dfs(0, i, heights, pacificVisited, rows, cols);
-            dfs(rows - 1, i, heights, atlanticVisited, rows, cols);
+        arr[i][j] = true;
+
+        mark(i + 1, j, arr, i, j, heights);
+        mark(i - 1, j, arr, i, j, heights);
+        mark(i, j + 1, arr, i, j, heights);
+        mark(i, j - 1, arr, i, j, heights);
+    }
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int n = heights.length;
+        int m = heights[0].length;
+
+        boolean[][] pacific = new boolean[n][m];
+        boolean[][] atlantic = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            mark(i, 0, pacific, -1, -1, heights);
+            mark(i, m - 1, atlantic, -1, -1, heights);
+        }
+
+        for (int i = 0; i < m; i++) {
+            mark(0, i, pacific, -1, -1, heights);
+            mark(n - 1, i, atlantic, -1, -1, heights);
         }
 
         List<List<Integer>> result = new ArrayList<>();
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (pacificVisited[i][j] && atlanticVisited[i][j]) {
-                    result.add(Arrays.asList(i, j));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(i);
+                    list.add(j);
+                    result.add(list);
                 }
             }
         }
 
         return result;
-    }
 
-    private void dfs(int x, int y, int[][] heights, boolean[][] visited, int rows, int cols) {
-        visited[x][y] = true;
-
-        int[][] directions = {
-            
-                {-1, 0}, {1, 0}, {0, -1}, {0, 1}
-            
-        };
-
-        for (int[] dir : directions) {
-            int newX = x + dir[0];
-            int newY = y + dir[1];
-
-            if (isValid(newX, newY, rows, cols) && !visited[newX][newY] 
-            && heights[newX][newY] >= heights[x][y]) {
-                dfs(newX, newY, heights, visited, rows, cols);
-            }
-        }
-    }
-
-    private boolean isValid(int x, int y, int rows, int cols) {
-        return x >= 0 && y >= 0 && x < rows && y < cols;
     }
 }
