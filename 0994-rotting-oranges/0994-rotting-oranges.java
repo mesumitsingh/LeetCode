09ea1140ae03008e3d232313ba7a114d71e282e8
Sumit Singh <144142094+mesumitsingh@users.noreply.github.com>
2026-08-05 19:59:03 +0530

    Time: 3 ms (9.99%), Space: 44.5 MB (13.75%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,11 +1,12 @@
 class Solution {
-    class Pair {
-        int first;
-        int second;
 
-        Pair(int x, int y) {
-            this.first = x;
-            this.second = y;
+    class Pair {
+        int a;
+        int b;
+
+        Pair(int a, int b) {
+            this.a = a;
+            this.b = b;
         }
     }
 
@@ -22,55 +23,58 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int ans = 0;
+        int t = 0;
+        boolean[][] vis = new boolean[n][m];
 
-        boolean[][] visited = new boolean[n][m];
-        Queue<Node> q = new LinkedList<>(); //((i, j), time)
+        Queue<Node> q = new LinkedList<>();
 
-        // pushing all sources in queue
+        // push all rotten to queue
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 2) {
                     q.add(new Node(new Pair(i, j), 0));
-                    visited[i][j] = true; 
+                    vis[i][j] = true;
                 }
             }
         }
 
+        int ans = 0;
+
         // bfs
-        while (q.size() > 0) {
-            int i = q.peek().p.first;
-            int j = q.peek().p.second;
+        while (!q.isEmpty()) {
+            int i = q.peek().p.a;
+            int j = q.peek().p.b;
             int time = q.peek().t;
             q.poll();
 
             ans = Math.max(ans, time);
 
-            if (i - 1 >= 0 && !visited[i - 1][j] && grid[i - 1][j] == 1) { // top  
+            if (i - 1 >= 0 && !vis[i - 1][j] && grid[i - 1][j] == 1) {
                 q.add(new Node(new Pair(i - 1, j), time + 1));
-                visited[i - 1][j] = true;
+                vis[i - 1][j] = true;
             }
 
-            if (i + 1 < n && !visited[i + 1][j] && grid[i + 1][j] == 1) { // botoom  
+            if (i + 1 < n && !vis[i + 1][j] && grid[i + 1][j] == 1) {
                 q.add(new Node(new Pair(i + 1, j), time + 1));
-                visited[i + 1][j] = true;
+                vis[i + 1][j] = true;
             }
 
-            if (j - 1 >= 0 && !visited[i][j - 1] && grid[i][j - 1] == 1) { // left  
+            if (j - 1 >= 0 && !vis[i][j - 1] && grid[i][j - 1] == 1) {
                 q.add(new Node(new Pair(i, j - 1), time + 1));
-                visited[i][j - 1] = true;
+                vis[i][j - 1] = true;
             }
 
-            if (j + 1 < m && !visited[i][j + 1] && grid[i][j + 1] == 1) { // right 
+            if (j + 1 < m && !vis[i][j + 1] && grid[i][j + 1] == 1) {
                 q.add(new Node(new Pair(i, j + 1), time + 1));
-                visited[i][j + 1] = true;
+                vis[i][j + 1] = true;
             }
+
         }
 
-        // check if fresh left 
+        // check if all are rotten
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1 && ! visited[i][j]) {
+                if (grid[i][j] == 1 && !vis[i][j]) {
                     return -1;
                 }
             }
